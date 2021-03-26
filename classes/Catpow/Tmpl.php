4b92002123b66396798abd65740ad896efa9c22b
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-26 17:40:03 +0900

    feat :+1: extract compile file process

--- classes/Catpow/Tmpl.php
+++ classes/Catpow/Tmpl.php
@@ -7,16 +7,24 @@ class Tmpl{
 			$html_file=substr($tmpl_file,0,-8).'html';
 			if(!file_exists($tmpl_file)){continue;}
 			if(!file_exists($html_file) or filemtime($html_file) < filemtime($tmpl_file)){
-				self::$tmpl=$tmpl_file;
-				self::$dir=dirname($tmpl_file);
-				ob_start();
-				include $tmpl_file;
-				file_put_contents($html_file,ob_get_clean());
-				echo "build {$html_file}\n";
-				touch($tmpl_file);
-				self::$tmpl=null;
-				self::$dir=null;
+				self::compile_file($tmpl_file,$html_file);
 			}
 		}
+	}
+	public static function compile_file($tmpl_file,$html_file){
+		self::$tmpl=$tmpl_file;
+		self::$dir=dirname($tmpl_file);
+		ob_start();
+		try{
+			include $tmpl_file;
+			file_put_contents($html_file,ob_get_clean());
+			printf("build %s\n",substr($html_file,strlen(ABSPATH)));
+		}
+		catch(\Error $e){
+			ob_end_clean();
+			echo $e->getMessage();
+		}
+		self::$tmpl=null;
+		self::$dir=null;
 	}
 }
