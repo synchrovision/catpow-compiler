b3cdac432dc23181b788614ab3f4fb50e9fda6c2
synchrovision <trinitybluster@yahoo.co.jp>
2021-03-26 17:41:10 +0900

    feat :+1: add picture and simple_md function

---
+++ includes/funcitons.php
@@ -0,0 +1,15 @@
+<?php
+namespace Catpow;
+
+function picture($name,$alt){
+	printf('<picture><source media="(max-width: 767px)" srcset="images/%1$s_sp.png"><img src="../about/images/%1$s.png" alt="%2$s"/></picture>',$name,$alt);
+}
+function simple_md($text,$param=[]){
+	$param=array_merge(
+		['link_class'=>'link','image_class'=>'image'],
+		$param
+	);
+	$text=preg_replace('/!\[(.+?)\]\((.+?)\)/','<img class="'.$param['image_class'].'" src="$2" alt="$1"/>',$text);
+	$text=preg_replace('/\[(.+?)\]\((.+?)\)/','<a class="'.$param['link_class'].'" href="$2" target="_brank">$1</a>',$text);
+	return $text;
+}
